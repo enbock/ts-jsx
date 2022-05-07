148d0f514f70def25e9267e25dd2ce7186939d4e
Endre Bock <dev@itbock.de>
2022-05-07 19:58:30 +0200

    Improve testing renderer

--- src/TestRenderer.ts
+++ src/TestRenderer.ts
@@ -1,11 +1,24 @@
 import ShadowRenderer from './ShadowRenderer';
 import {ShadowDomElement} from './ShadowDom';
+import Component from './Component';
 
 export default class TestRenderer {
-    public static render(jsx: ShadowDomElement):HTMLElement {
-        const view: HTMLElement = ShadowRenderer.render(jsx);
-        const container: HTMLElement = document.createElement('container');
-        for (const shadow of (view.shadowRoot as any).shadowNodes) container.appendChild(shadow.domNode);
-        return container;
+    public static render(jsx: ShadowDomElement): HTMLElement {
+        const view: any = ShadowRenderer.render(jsx);
+        view.renderer = this;
+        TestRenderer.updateContainer(view);
+        return view as HTMLElement;
+    }
+
+    public static renderShadow(component: Component): void {
+        ShadowRenderer.renderShadow(component);
+        TestRenderer.updateContainer(component);
+    }
+
+    private static updateContainer(view: any) {
+        while (view.children.length) {
+            view.removeChild(view.firstChild!);
+        }
+        for (const shadow of (view.shadowRoot as any).shadowNodes) view.appendChild(shadow.domNode);
     }
 }
