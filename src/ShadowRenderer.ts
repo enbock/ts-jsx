432c5bc4faa64c8302e95a5161b4f61ec050c461
Endre Bock <endre@itbock.de>
2024-02-22 14:13:15 +0100

    Correct handling of select in case of reusing existing element with other data.

--- src/ShadowRenderer.ts
+++ src/ShadowRenderer.ts
@@ -59,8 +59,17 @@ export default class ShadowRenderer {
             (domNode as Component).updateProps(result.props);
             return;
         }
-        if (result.tagName === 'input' && result.props.hasOwnProperty('value')) {
-            (domNode as HTMLInputElement).value = result.props.value;
+        if (domNode.hasOwnProperty('value') && result.props.hasOwnProperty('value')) {
+            (<HTMLInputElement>domNode).value = result.props.value;
+        }
+        if(domNode.tagName.toUpperCase() == 'SELECT' && result.props.hasOwnProperty('value')) {
+            const options:HTMLCollectionOf<HTMLOptionElement> = <HTMLCollectionOf<HTMLOptionElement>>domNode.getElementsByTagName('OPTION');
+            for(let i=0; i< options.length; i++) {
+                if (result.props.value != options[i].value) continue;
+                if(domNode.hasOwnProperty('selectedValue'))
+                    (<any>domNode).selectedValue = result.props.value;
+                (<HTMLSelectElement>domNode).selectedIndex = i;
+            }
         }
         for (const key of Object.keys(result.props)) {
             const isOnDashStyle: boolean = key.substring(0, 3) == 'on-';
